ef49bbff6640a42220e6d17d114303de2e13f5c5
Fakhriddin <fakhmirakov1998@gmail.com>
2023-03-01 23:33:34 +0500

    homework for lesson 3

---
+++ homeworks/lesson3.js
@@ -0,0 +1,48 @@
+let arr = [
+  {
+    name: 'alex',
+    age: 18 
+  },
+  {
+    name: 'refat',
+    age: 19
+  },
+  {
+    name: 'amal',
+    age: 14
+  },
+  {
+    name: 'shoxrux',
+    age: 17
+  },
+  {
+    name: 'abdulvosid',
+    age: 16
+  },
+  {
+    name: 'jamshed',
+    age: 18
+  },
+  {
+    name: 'manuchexr',
+    age: 18
+  },
+  {
+    name: 'Faxriddin',
+    age: 24
+  },
+  {
+    name: 'Shakhnoza',
+    age: 15
+  },
+  {
+    name: 'Timur',
+    age: 17
+  },
+]
+
+let overEightTeen = arr.filter(item => item.age >= 18)
+console.log(overEightTeen);
+
+let underEightTeen = arr.filter(item => item.age < 18)
+console.log(underEightTeen);
